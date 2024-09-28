33643b1f81e18c9fbf2bac766c9043c425e4f43c
Miguel Closa <jmrclosa@gmail.com>
2024-09-27 22:50:19 -0400

    Enter 'npm install firebase', begin creating todo list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Sky from "./components/Sky.js";
 
 function App() {
   return (
 
       // Place elements here
 
-      <>
+      <div className="App">
+          <>
+              <Sky/>
+          </>
 
+      </div>
 
-
-      </>
 
   );
 }
--- src/components/Sky.js
+++ src/components/Sky.js
@@ -6,6 +6,20 @@ import "../styles/skySheet.css";
 function Sky(){
 
 
+    return (
+
+        <div className="sky-container">
+
+            <h1 id="title">Nurture</h1>
+
+
+
+        </div>
+
+
+    )
+
+
 
 
 }
